58d8e2e37c24520b10da54a794fbe3bb873faad7
Jordan <jchau3@buffalo.edu>
2018-02-10 10:11:14 -0500

    The basic features of the app are fully functional, fixed potential app crashes with toast messages.

--- app/src/main/java/jay412/grip/MainActivity.java
+++ app/src/main/java/jay412/grip/MainActivity.java
@@ -2,15 +2,27 @@ package jay412.grip;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etTotalMealCost, etTax, etTips, etCustomTips;
-    private Button btnCalculateTax, btn15Tips, btn20Tips, btnCustomTips;
+    private EditText etTotalMealCost, etTax, etTips, etCustomTips, etPeopleShare;
+    private Button btnCalculateTax, btn15Tips, btn20Tips, btnCustomTips, btnCalculateGrandTotal;
+    private TextView tvGrandTotal;
 
-    private double mealCost, tax, tips;
+    private double mealCost, tax, tips, numPeople, grandTotal;
+
+    /*TO-DO:
+    * tax field is editable b/c different places have different tax rates
+
+      */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +33,19 @@ public class MainActivity extends AppCompatActivity {
         etTax = findViewById(R.id.etTax);
         etTips = findViewById(R.id.etTips);
         etCustomTips = findViewById(R.id.etCustomTip);
+        etPeopleShare = findViewById(R.id.etPeopleShare);
+        tvGrandTotal = findViewById(R.id.tvGrandTotalValue);
 
         btnCalculateTax = findViewById(R.id.btnCalcTax);
         btnCalculateTax.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mealCost = Double.valueOf(etTotalMealCost.getText().toString());
-                calculateTax(mealCost);
+                if(TextUtils.isEmpty(etTotalMealCost.getText().toString()))
+                    Toast.makeText(MainActivity.this, "Please specify a value for Total Meal Cost.", Toast.LENGTH_LONG).show();
+                else {
+                    mealCost = Double.valueOf(etTotalMealCost.getText().toString());
+                    calculateTax(mealCost);
+                }
             }
         });
 
@@ -51,21 +69,65 @@ public class MainActivity extends AppCompatActivity {
         btnCustomTips.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double tipPercent = Double.valueOf(etCustomTips.getText().toString());
-                calculateTips(mealCost, tax, tipPercent);
+                if(TextUtils.isEmpty(etCustomTips.getText().toString()))
+                    Toast.makeText(MainActivity.this, "Please specify a value for Custom Tips.", Toast.LENGTH_LONG).show();
+                else {
+                    double tipPercent = Double.valueOf(etCustomTips.getText().toString());
+                    calculateTips(mealCost, tax, tipPercent);
+                }
+            }
+        });
+
+        btnCalculateGrandTotal = findViewById(R.id.btnCalculateTotal);
+        btnCalculateGrandTotal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numPeople = Double.valueOf(etPeopleShare.getText().toString());
+                calculateTotal(mealCost, tax, tips, numPeople);
             }
         });
     }
 
     private void calculateTax(double mCost){
         tax = mCost * 0.08875;
+        roundToMoneyFormat(tax);
         String calcTax = Double.toString(tax);
         etTax.setText(calcTax);
     }
 
     private void calculateTips(double mCost, double mealTax, double percent){
-        tips = (mCost + mealTax) * percent;
-        String calcTips = Double.toString(tips);
-        etTips.setText(calcTips);
+        if(TextUtils.isEmpty(etTotalMealCost.getText().toString()))
+            Toast.makeText(MainActivity.this, "Please specify a value for Total Meal Cost.", Toast.LENGTH_LONG).show();
+        else if(TextUtils.isEmpty(etTax.getText().toString()))
+            Toast.makeText(MainActivity.this, "Tax has not been calculated yet.", Toast.LENGTH_LONG).show();
+        else {
+            tips = (mCost + mealTax) * percent;
+            roundToMoneyFormat(tips);
+            String calcTips = Double.toString(tips);
+            etTips.setText(calcTips);
+        }
+    }
+
+    private void calculateTotal(double mealCost, double mealTax, double tips, double numPeople){
+        if(TextUtils.isEmpty(etTotalMealCost.getText().toString()))
+            Toast.makeText(MainActivity.this, "Please specify a value for Total Meal Cost.", Toast.LENGTH_LONG).show();
+        else if(TextUtils.isEmpty(etTax.getText().toString()))
+            Toast.makeText(MainActivity.this, "Tax has not been calculated yet.", Toast.LENGTH_LONG).show();
+        else if(TextUtils.isEmpty(etTips.getText().toString()))
+            Toast.makeText(MainActivity.this, "Tips has not been entered or calculated yet.", Toast.LENGTH_LONG).show();
+        else if (etPeopleShare.getText().toString().equals("0")){
+            Toast.makeText(MainActivity.this, "Invalid number of people.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            grandTotal = (mealCost + mealTax + tips) / numPeople;
+            roundToMoneyFormat(grandTotal);
+            tvGrandTotal.setText("$" + Double.toString(grandTotal));
+        }
+    }
+
+    public String roundToMoneyFormat(double m){
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+        return df.format(m);
     }
 }
