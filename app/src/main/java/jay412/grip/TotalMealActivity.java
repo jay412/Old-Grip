0dde4751dd2aeb96c69b2fa2317bdcb7b7e4e87a
Jordan Chau <jchau3@buffalo.edu>
2018-04-16 18:51:44 -0400

    1) changed min sdk to 21 2) created a dialog for adding a new item and price

--- app/src/main/java/jay412/grip/TotalMealActivity.java
+++ app/src/main/java/jay412/grip/TotalMealActivity.java
@@ -1,14 +1,26 @@
 package jay412.grip;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TotalMealActivity extends AppCompatActivity {
+
+    private int[] mPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +34,47 @@ public class TotalMealActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_menu, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id == android.R.id.home) {
             NavUtils.navigateUpFromSameTask(this);
         } else if(id == R.id.action_add) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(TotalMealActivity.this);
+            LayoutInflater inflater = getLayoutInflater();
 
+            final View dialogView = inflater.inflate(R.layout.dialog_template, null);
+            final EditText mItem = dialogView.findViewById(R.id.et_item);
+            final EditText mPrice = dialogView.findViewById(R.id.et_price);
+
+            builder.setTitle("Add A New Item");
+            builder.setView(dialogView)
+                    .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //add button
+                            String itemName = mItem.getText().toString();
+                            String price = mPrice.getText().toString();
+                            Log.i("Item Name: ", itemName);
+                            Log.i("Price: ", price);
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+            final AlertDialog dialog = builder.create();
+            dialog.show();
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.add_menu, menu);
-        return true;
     }
 }
